641b4b3495ed00b8864236b249f03f9ccc1814d7
Davey LeMieux <dlemieux2@snapchat.com>
2020-06-22 23:23:18 -0700

    More testing and cleanup

--- tests/timing.js
+++ tests/timing.js
@@ -7,23 +7,23 @@ const testTiming = () => {
 
   const startTimer = performance.now();
 
-  for (var i = 0; i < 1000000; ++i) {
+  for (let i = 0; i < 1000000; ++i) {
     worker.getId("test agent");
   }
 
-  var endTimer = performance.now();
+  const endTimer = performance.now();
 
   console.log(endTimer - startTimer, "milliseconds");
 }
 
 const testUniqueness = () => {
-  console.log("Timing Test - generating 1 million IDs and testing for uniqueness");
+  console.log("Uniqueness Test - generating 1 million IDs and testing for uniqueness");
 
 
   const startTimer = performance.now();
 
   const idSet = new Set();
-  for (var i = 0; i < 1000000; ++i) {
+  for (let i = 0; i < 1000000; ++i) {
     const id = worker.getId("test agent");
     if (idSet.has(id)) {
       throw new Error("DUPLICATE ID FOUND");
@@ -32,7 +32,38 @@ const testUniqueness = () => {
     idSet.add(id);
   }
 
-  var endTimer = performance.now();
+  const endTimer = performance.now();
+  console.log("Timing Test - generating 1 million IDs");
+  console.log("generated", idSet.size, "unique ids");
+  console.log(endTimer - startTimer, "milliseconds");
+}
+
+const testMultiServer = () => {
+  console.log("Uniqueness Test - generating 1 million IDs with 5 workers");
+
+  const worker1 = idworker.getIdWorker(1, 0);
+  const worker2 = idworker.getIdWorker(2, 0);
+  const worker3 = idworker.getIdWorker(3, 0);
+  const worker4 = idworker.getIdWorker(4, 0);
+  const worker5 = idworker.getIdWorker(5, 0);
+
+  const workers = [worker, worker1, worker2, worker3, worker4, worker5];
+
+  const startTimer = performance.now();
+
+  const idSet = new Set();
+  for (let i = 0; i < 1000000; ++i) {
+    for (const worker of workers) {
+      const id = worker.getId("test agent");
+      idSet.add(id);
+    }
+  }
+
+  if (idSet.size !== 1000000 * workers.length) {
+    throw new Error('ID Set not large enough');
+  }
+
+  const endTimer = performance.now();
   console.log("Timing Test - generating 1 million IDs");
   console.log("generated", idSet.size, "unique ids");
   console.log(endTimer - startTimer, "milliseconds");
@@ -40,3 +71,4 @@ const testUniqueness = () => {
 
 testTiming();
 testUniqueness();
+testMultiServer();
